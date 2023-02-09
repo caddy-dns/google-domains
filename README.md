0afaddefff0a29e36214ee99d8e9ce97b19c4a32
Matthew Holt <mholt@users.noreply.github.com>
2023-02-09 13:16:28 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
-**DEVELOPER INSTRUCTIONS:**
-
-- Update module name in go.mod
-- Update dependencies to latest versions
-- Update name and year in license
-- Customize configuration and Caddyfile parsing
-- Update godocs / comments (especially provider name and nuances)
-- Update README and remove this section
-
----
-
-\<PROVIDER\> module for Caddy
+Google Domains module for Caddy
 ===========================
 
-This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records with \<PROVIDER\>.
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage ACME DNS challenge records with Google Domains.
+
+**Unlike most DNS provider modules for Caddy, this module works ONLY for ACME DNS challenges, due to limitations in the Google Domains API, which is designed only for manipulating TXT records for the DNS challenge.**
 
 ## Caddy module name
 
 ```
-dns.providers.provider_name
+dns.providers.google_domains
 ```
 
 ## Config examples
@@ -30,8 +21,8 @@ To use this module for the ACME DNS challenge, [configure the ACME issuer in you
 	"challenges": {
 		"dns": {
 			"provider": {
-				"name": "provider_name",
-				"api_token": "YOUR_PROVIDER_API_TOKEN"
+				"name": "google_domains",
+				"access_token": "YOUR_ACCESS_TOKEN"
 			}
 		}
 	}
@@ -43,13 +34,13 @@ or with the Caddyfile:
 ```
 # globally
 {
-	acme_dns provider_name ...
+	acme_dns google_domains <access_token>
 }
 ```
 
 ```
 # one site
 tls {
-	dns provider_name ...
+	dns google_domains <access_token>
 }
 ```
--- module.go
+++ module.go
@@ -1,76 +1,180 @@
-package template
+package googledomains
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	libdnstemplate "github.com/libdns/template"
+	"github.com/caddyserver/certmagic"
+	"github.com/libdns/libdns"
 )
-
-// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
-type Provider struct{ *libdnstemplate.Provider }
 
 func init() {
 	caddy.RegisterModule(Provider{})
 }
 
+// Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
+type Provider struct {
+	AccessToken        string `json:"access_token,omitempty"`
+	KeepExpiredRecords bool   `json:"keep_expired_records,omitempty"`
+}
+
 // CaddyModule returns the Caddy module information.
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "dns.providers.template",
-		New: func() caddy.Module { return &Provider{new(libdnstemplate.Provider)} },
+		ID:  "dns.providers.google_domains",
+		New: func() caddy.Module { return new(Provider) },
 	}
 }
 
-// TODO: This is just an example. Useful to allow env variable placeholders; update accordingly.
 // Provision sets up the module. Implements caddy.Provisioner.
 func (p *Provider) Provision(ctx caddy.Context) error {
-	p.Provider.APIToken = caddy.NewReplacer().ReplaceAll(p.Provider.APIToken, "")
-	return fmt.Errorf("TODO: not implemented")
+	p.AccessToken = caddy.NewReplacer().ReplaceAll(p.AccessToken, "")
+	return nil
 }
 
-// TODO: This is just an example. Update accordingly.
+// AppendRecords adds records to the zone. It returns the records that were added.
+func (p *Provider) AppendRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	return p.acmeTXTRecordAPIRequest(ctx, zone, records, "add")
+}
+
+// DeleteRecords deletes the records from the zone. It returns the records that were deleted.
+func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	return p.acmeTXTRecordAPIRequest(ctx, zone, records, "remove")
+}
+
+func (p *Provider) acmeTXTRecordAPIRequest(ctx context.Context, zone string, records []libdns.Record, addOrRemove string) ([]libdns.Record, error) {
+	payload, err := p.makePayload(zone, records, addOrRemove)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := doRequest(ctx, zone, payload)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if err := handleResponse(resp); err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func (p *Provider) makePayload(zone string, records []libdns.Record, addOrRemove string) (rotateChallengesBody, error) {
+	if addOrRemove != "add" && addOrRemove != "remove" {
+		return rotateChallengesBody{}, fmt.Errorf("can only add or remove; unrecognized: %s", addOrRemove)
+	}
+
+	// TODO: the Google Domains API is very limited in what kinds of records it supports; return error if unsupported
+
+	payload := rotateChallengesBody{
+		AccessToken:        p.AccessToken,
+		KeepExpiredRecords: p.KeepExpiredRecords,
+	}
+
+	// choose the correct field on the struct to which to append records
+	dest := &payload.RecordsToAdd
+	if addOrRemove == "remove" {
+		dest = &payload.RecordsToRemove
+	}
+
+	// convert incoming record types to the format the API requires
+	for _, rec := range records {
+		*dest = append(*dest, acmeTXTRecord{
+			FQDN:   libdns.AbsoluteName(rec.Name, zone),
+			Digest: rec.Value,
+		})
+	}
+
+	return payload, nil
+}
+
+func doRequest(ctx context.Context, zone string, payload rotateChallengesBody) (*http.Response, error) {
+	uri := fmt.Sprintf("%s%s:rotateChallenges", apiBase, zone)
+
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, uri, bytes.NewReader(payloadJSON))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	return http.DefaultClient.Do(req)
+}
+
+func handleResponse(resp *http.Response) error {
+	if resp.StatusCode == http.StatusOK {
+		return nil
+	}
+
+	var info errorResponse
+	err := json.NewDecoder(resp.Body).Decode(&info)
+	if err != nil {
+		return fmt.Errorf("reading error body: %v", err)
+	}
+
+	return fmt.Errorf("HTTP %d: %s", resp.StatusCode, info.Error.Message)
+}
+
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-// providername [<api_token>] {
-//     api_token <api_token>
-// }
-//
-// **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
+//	google_domains <access_token>
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
-		if d.NextArg() {
-			p.Provider.APIToken = d.Val()
+		if !d.NextArg() {
+			return d.ArgErr()
 		}
+		p.AccessToken = d.Val()
 		if d.NextArg() {
 			return d.ArgErr()
 		}
-		for nesting := d.Nesting(); d.NextBlock(nesting); {
-			switch d.Val() {
-			case "api_token":
-				if p.Provider.APIToken != "" {
-					return d.Err("API token already set")
-				}
-				if d.NextArg() {
-					p.Provider.APIToken = d.Val()
-				}
-				if d.NextArg() {
-					return d.ArgErr()
-				}
-			default:
-				return d.Errf("unrecognized subdirective '%s'", d.Val())
-			}
-		}
-	}
-	if p.Provider.APIToken == "" {
-		return d.Err("missing API token")
 	}
 	return nil
 }
 
+type rotateChallengesBody struct {
+	AccessToken        string          `json:"accessToken"`
+	RecordsToAdd       []acmeTXTRecord `json:"recordsToAdd,omitempty"`
+	RecordsToRemove    []acmeTXTRecord `json:"recordsToRemove,omitempty"`
+	KeepExpiredRecords bool            `json:"keepExpiredRecords,omitempty"`
+}
+
+type acmeTXTRecord struct {
+	FQDN       string `json:"fqdn"`
+	Digest     string `json:"digest"`
+	UpdateTime string `json:"updateTime,omitempty"`
+}
+
+type errorResponse struct {
+	Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Status  string `json:"status"`
+		Details []struct {
+			Type            string `json:"@type"`
+			FieldViolations []struct {
+				Description string `json:"description"`
+			} `json:"fieldViolations"`
+		} `json:"details"`
+	} `json:"error"`
+}
+
+// API Reference: https://developers.google.com/domains/acme-dns/reference/rest
+const apiBase = "https://acmedns.googleapis.com/v1/acmeChallengeSets/"
+
 // Interface guards
 var (
-	_ caddyfile.Unmarshaler = (*Provider)(nil)
-	_ caddy.Provisioner     = (*Provider)(nil)
+	_ caddyfile.Unmarshaler     = (*Provider)(nil)
+	_ caddy.Provisioner         = (*Provider)(nil)
+	_ certmagic.ACMEDNSProvider = (*Provider)(nil)
 )
